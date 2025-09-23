0a9148276a8ffa253ef7a4acc89abc9ea4231439
qowlgo00 <qowlgo00@gmail.com>
2025-09-23 18:02:18 +0900

    feat: ColorList 컴포넌트 틀 제작

--- Ch6_ReactStateManagement/react-codeground/src/App.jsx
+++ Ch6_ReactStateManagement/react-codeground/src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
+import { useState } from 'react'
+
+import colorData from './data/color-data.json'
+import ColorList from './components/ColorList';
 import StarRating from './components/StarRating'
 
 function App() {
+  const [colors] = useState(colorData);
+
   return (
     <>
+      <ColorList/>
       <StarRating
-        style={{background: "pink"}}
+        style={{background: "pink"}}S
         totalStars={9}
         onDoubleClick={e => alert("double click!!")}
       />
