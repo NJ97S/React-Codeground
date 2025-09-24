2a8c35d638d8ecff535ed374168a1052998b3323
qowlgo00 <qowlgo00@gmail.com>
2025-09-24 11:29:35 +0900

    feat: AddColorForm 의 form 구현 및 참조 사용

--- Ch6_ReactStateManagement/react-codeground/src/components/AddColorForm.jsx
+++ Ch6_ReactStateManagement/react-codeground/src/components/AddColorForm.jsx
@@ -1,3 +1,28 @@
-export default function AddColorForm(params) {
-    
+import { useRef } from "react";
+
+export default function AddColorForm({onNewColor = () => {}}) {
+    const txtTitle = useRef();
+    const hexColor = useRef();
+
+    const submit = e => {
+        // 디폴트 동작인 현재 URL로 폼 엘리먼트에 저장된 값이 본문에 들어 있는 POST 요청을 보내는 것을 막기 위함.
+        e.preventDefault();
+
+        const title = txtTitle.current.value;
+        const color = hexColor.current.value;
+
+        onNewColor(title, color);
+        
+        // 참조 타입을 사용해 DOM 노드의 속성을 직접 변경 가능
+        txtTitle.current.value = "";
+        hexColor.current.value = "";
+    };
+
+    return (
+        <form onSubmit={submit}>
+            <input type="text" ref={txtTitle} placeholder="color title..." required/>
+            <input type="color" ref={hexColor} required/>
+            <button>ADD</button>
+        </form>
+    );
 };
