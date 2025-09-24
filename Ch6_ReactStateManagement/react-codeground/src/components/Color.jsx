993741aae30126cece47341a4db5227d5fa2a194
qowlgo00 <qowlgo00@gmail.com>
2025-09-24 11:10:25 +0900

    fix: 데이터 삭제 오류 수정

--- Ch6_ReactStateManagement/react-codeground/src/components/Color.jsx
+++ Ch6_ReactStateManagement/react-codeground/src/components/Color.jsx
@@ -1,7 +1,7 @@
 import { FaTrash } from "react-icons/fa"
 import StarRating from "./StarRating"
 
-export default function Color({ title, color, rating, onRemove = () => {}}) {
+export default function Color({ id, title, color, rating, onRemove = () => {}}) {
     return (
         <section>
             <h1>{title}</h1>
