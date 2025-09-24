cad28df5e3251e62d9435a08ba4c6648855db152
qowlgo00 <qowlgo00@gmail.com>
2025-09-24 13:02:45 +0900

    refactoring: useInput 적용하여 useState로 구현한 코드와 중복 코드 줄이기

--- Ch6_ReactStateManagement/react-codeground/src/components/AddColorForm.jsx
+++ Ch6_ReactStateManagement/react-codeground/src/components/AddColorForm.jsx
@@ -1,8 +1,8 @@
-import { useRef, useState } from "react";
+import useInput from "../hooks/useInput";
 
 export default function AddColorForm({onNewColor = () => {}}) {
-    const [title, setTitle] = useState("");
-    const [color, setColor] = useState("#000000");
+    const [titleProps, resetTitle] = useInput("");
+    const [colorProps, resetColor] = useInput("#000000");
 
     const submit = e => {
         // 디폴트 동작인 현재 URL로 폼 엘리먼트에 저장된 값이 본문에 들어 있는 POST 요청을 보내는 것을 막기 위함.
@@ -10,21 +10,21 @@ export default function AddColorForm({onNewColor = () => {}}) {
 
         onNewColor(title, color);
         
-        setTitle("");
-        setColor("#000000");
+        resetTitle("");
+        resetColor("#000000");
     };
 
     return (
         <form onSubmit={submit}>
-            <input 
+            <input
+                {...titleProps}
                 type="text" 
                 placeholder="color title..." 
-                onChange={e => setTitle(e.target.value)}
                 required
             />
             <input 
+                {...colorProps}
                 type="color"
-                onChange={e => setColor(e.target.value)}
                 required/>
             <button>ADD</button>
         </form>
