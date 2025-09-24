866de2760a13bde252b596bae0b5362eb7a967b6
qowlgo00 <qowlgo00@gmail.com>
2025-09-24 11:33:59 +0900

    refactoring: 제어가 되는 컴포넌트로 useRef에서 useState로 로직 변경

--- Ch6_ReactStateManagement/react-codeground/src/components/AddColorForm.jsx
+++ Ch6_ReactStateManagement/react-codeground/src/components/AddColorForm.jsx
@@ -1,27 +1,31 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function AddColorForm({onNewColor = () => {}}) {
-    const txtTitle = useRef();
-    const hexColor = useRef();
+    const [title, setTitle] = useState("");
+    const [color, setColor] = useState("#000000");
 
     const submit = e => {
         // 디폴트 동작인 현재 URL로 폼 엘리먼트에 저장된 값이 본문에 들어 있는 POST 요청을 보내는 것을 막기 위함.
         e.preventDefault();
 
-        const title = txtTitle.current.value;
-        const color = hexColor.current.value;
-
         onNewColor(title, color);
         
-        // 참조 타입을 사용해 DOM 노드의 속성을 직접 변경 가능
-        txtTitle.current.value = "";
-        hexColor.current.value = "";
+        setTitle("");
+        setColor("#000000");
     };
 
     return (
         <form onSubmit={submit}>
-            <input type="text" ref={txtTitle} placeholder="color title..." required/>
-            <input type="color" ref={hexColor} required/>
+            <input 
+                type="text" 
+                placeholder="color title..." 
+                onChange={e => setTitle(e.target.value)}
+                required
+            />
+            <input 
+                type="color"
+                onChange={e => setColor(e.target.value)}
+                required/>
             <button>ADD</button>
         </form>
     );
