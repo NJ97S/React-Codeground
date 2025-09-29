1587dc6b1f35a773af6c0c3f6f7cb81619a4d0f6
HyoJin <cron.phj@gmail.com>
2025-09-30 00:38:41 +0900

    🎨 style: Prettier 프로젝트 일괄 적용

--- Hyojin/react-state-management/src/App.jsx
+++ Hyojin/react-state-management/src/App.jsx
@@ -1,12 +1,7 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  return (
-    <>
-      <h1>React-state-management</h1>
-    </>
-  )
+  return <h1>React-state-management</h1>;
 }
 
-export default App
+export default App;
