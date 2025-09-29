0a6144becda82747d17783c70f84b7319bd0354b
HyoJin <cron.phj@gmail.com>
2025-09-30 00:30:43 +0900

    ✨ feat: 프로젝트 처음부터 시작을 위해 모든 기능 제거

--- Hyojin/react-state-management/src/App.jsx
+++ Hyojin/react-state-management/src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
-import ColorList from './components/ColorList'
 
 function App() {
 
   return (
     <>
       <h1>React-state-management</h1>
-      <ColorList />
     </>
   )
 }
--- Hyojin/react-state-management/src/components/Star.jsx
+++
@@ -1,9 +0,0 @@
-import {FaStar} from "react-icons/fa"
-
-export default function Star({selected = false, onClicked = () => {console.log("클릭됐다임마")}}) {
-  return (
-    <>
-      <FaStar color={selected ? "red" : "grey"} onClick={onClicked}/>
-    </>
-  )
-}
