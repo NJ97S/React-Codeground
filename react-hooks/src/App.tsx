10d383782a0250f23ab6472ec878e3d78c9d7d1b
소남주 <namjuso1228@gmail.com>
2025-09-30 17:30:18 +0900

    feat: 중간 저장

--- react-hooks/src/App.tsx
+++ react-hooks/src/App.tsx
@@ -1,9 +1,33 @@
-import Checkbox from "./components/Checkbox";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [val, set] = useState("");
+  const [phrase, setPhrase] = useState("example phrase");
+
+  const createPhrase = () => {
+    setPhrase(val);
+    set("");
+  };
+
+  useEffect(() => {
+    console.log(`typing "${val}"`);
+  }, [val]);
+
+  useEffect(() => {
+    console.log(`saved phrase: "${phrase}"`);
+  }, [phrase]);
+
   return (
     <>
-      <Checkbox />
+      <label htmlFor="phrase">Favorite phrase:</label>
+      <input
+        id="phrase"
+        type="text"
+        value={val}
+        placeholder={phrase}
+        onChange={(e) => set(e.target.value)}
+      />
+      <button onClick={createPhrase}>send</button>
     </>
   );
 }
