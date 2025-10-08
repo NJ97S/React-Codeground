a8705a8a689cb21d08683b8482e7c9e76a1763aa
HyoJin <cron.phj@gmail.com>
2025-10-08 14:07:51 +0900

    ♻️ refactor: Array 생성 함수에서 Array.from 으로 리팩토링

--- Hyojin/react-state-management/src/components/star/StarRating.jsx
+++ Hyojin/react-state-management/src/components/star/StarRating.jsx
@@ -1,6 +1,5 @@
 import Star from "./Star";
 
 export default function StarRating({ totalStars = 5 }) {
-  const createStarArray = (length) => [...Array(length)];
-  return createStarArray(totalStars).map((n, i) => <Star key={i} />);
+  return Array.from({ length: totalStars }, (_, i) => <Star key={i} />);
 }
