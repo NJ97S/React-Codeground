422ea19c59fea74dea837f32dcbf7181ad121cfd
HyoJin <cron.phj@gmail.com>
2025-10-15 01:52:51 +0900

    ✨ feat: Input 커스텀 hook 통합

--- Hyojin/react-state-management/src/components/color/AddColorForm.jsx
+++ Hyojin/react-state-management/src/components/color/AddColorForm.jsx
@@ -1,31 +1,20 @@
-import { useState } from "react";
+import { useInput } from "../../hooks/useInput";
 
 export default function AddColorForm({ onNewColorInfo = (f) => f }) {
-  const [newTitle, setNewTitle] = useState("");
-  const [newColor, setNewColor] = useState("#000000");
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("#000000");
 
   const submitNewColor = (e) => {
     e.preventDefault();
-    onNewColorInfo(newTitle, newColor);
-    setNewTitle("");
-    setNewColor("#000000");
+    onNewColorInfo(titleProps.value, colorProps.value);
+    resetTitle();
+    resetColor();
   };
 
   return (
     <form onSubmit={submitNewColor}>
-      <input
-        type="text"
-        value={newTitle}
-        onChange={(e) => setNewTitle(e.target.value)}
-        placeholder="색깔 이름"
-        required
-      />
-      <input
-        type="color"
-        value={newColor}
-        onChange={(e) => setNewColor(e.target.value)}
-        required
-      />
+      <input type="text" {...titleProps} placeholder="색깔 이름" required />
+      <input type="color" {...colorProps} required />
       <button>추가하기</button>
     </form>
   );
