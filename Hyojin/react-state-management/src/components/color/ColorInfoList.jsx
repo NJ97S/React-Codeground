c21b106d09a07b69d88d18288f2d5cce3d1b36fb
박효진 <cron.phj@gmail.com>
2025-10-15 16:48:41 +0900

    ✨ feat: ContextProvider를 통한 데이터 전달 로직 추가

--- Hyojin/react-state-management/src/components/color/ColorInfoList.jsx
+++ Hyojin/react-state-management/src/components/color/ColorInfoList.jsx
@@ -1,11 +1,15 @@
+import { useContext } from "react";
+
+import { ColorContext } from "../../main";
 import Color from "./Color";
 
 export default function ColorInfoList({
-  colors = [],
   onRemoveColor = (f) => f,
   onRateColor = (f) => f,
 }) {
+  const { colors } = useContext(ColorContext);
   if (!colors.length) return <div>표시할 색상이 없습니다.</div>;
+
   return (
     <div>
       {colors.map((color) => (
--- Hyojin/react-state-management/src/main.jsx
+++ Hyojin/react-state-management/src/main.jsx
@@ -1,5 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { createContext } from "react";
+
 import "./index.css";
 import App from "./App.jsx";
+import colordata from "./data/color-data.json";
 
-createRoot(document.getElementById("root")).render(<App />);
+export const ColorContext = createContext();
+
+createRoot(document.getElementById("root")).render(
+  <ColorContext.Provider value={{ colors: colordata.colors }}>
+    <App />
+  </ColorContext.Provider>
+);
